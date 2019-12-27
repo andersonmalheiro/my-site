2a797553813aa19355025d485045eefe10969bfc
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-12-27 08:23:56 -0300

    Updating manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Anderson Malheiro de Carvalho`,
+        short_name: `Portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#3b5094`,
+        theme_color: `#3b5094`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
@@ -31,4 +31,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
