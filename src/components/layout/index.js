21c900a6a366b39fdff04bd045fa744e31fb8d8d
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2020-02-10 23:53:30 -0300

    Refactor on layout component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,39 +1,100 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
-
-import Header from '../header';
-import './layout.css';
-import { FaHeart } from 'react-icons/fa';
+import './layout.scss';
+import Helmet from 'react-helmet';
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+import {
+  MdHome,
+  MdInfo,
+  MdDeveloperBoard,
+  MdEmail,
+  MdMessage,
+} from 'react-icons/md';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+  const [showSocial, setShowSocial] = useState(false);
+
+  const toggleSocial = () => {
+    setShowSocial(!showSocial);
+  };
 
   return (
-    <div style={{ position: 'relative' }}>
-      <Header siteTitle={data.site.siteMetadata.title} />
+    <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"
+        />
+      </Helmet>
       <div className="wrapper">
-        <main className="main">{children}</main>
-        <footer className="footer">
-          © {new Date().getFullYear()}, Anderson Malheiro de Carvalho
-        </footer>
+        {children}
+
+        <div className="floatingButton" onClick={() => toggleSocial()}>
+          <MdMessage className="icon" />
+        </div>
+
+        {showSocial && (
+          <React.Fragment>
+            <a
+              href="https://www.instagram.com/_anderson_97"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={
+                `social bottom ${showSocial ? 'fade-in-bottom' : ''}`
+              }
+            >
+              <FaInstagram className="icon" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/andersonmalheiro"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={
+                `social mid ${showSocial ? 'fade-in-bottom' : ''}`
+              }
+            >
+              <FaLinkedin className="icon" />
+            </a>
+            <a
+              href="https://github.com/andersonmalheiro"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={
+                `social midder ${showSocial ? 'fade-in-bottom' : ''}`
+              }
+            >
+              <FaGithub className="icon" />
+            </a>
+            <a
+              href="mailto:malheiroanderson97@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={
+                `social top ${showSocial ? 'fade-in-bottom' : ''}`
+              }
+            >
+              <MdEmail className="icon" />
+            </a>
+          </React.Fragment>
+        )}
+        <div className="bottomMenu">
+          <div className="links">
+            <a href="#home">
+              <MdHome className="icon" />
+            </a>
+            <a href="#about">
+              <MdInfo className="icon" />
+            </a>
+            <a href="#portfolio">
+              <MdDeveloperBoard className="icon" />
+            </a>
+            <div onClick={() => toggleSocial()}>
+              <MdMessage className="icon" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
